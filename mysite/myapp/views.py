50bc1cbe198315475cabc65879608566ec359fae
Your Name <you@example.com>
2024-10-15 20:13:54 +0300

    Initialized

---
+++ mysite/myapp/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+from .models import Product
+
+
+
+def index(request):
+
+    products = Product.objects.all()
+
+    context = {
+        "products": products
+    }
+    return render(request, "myapp/index.html", context)
